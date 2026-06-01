52874976ea74930edf91e45d3cde04d9a797b5dd
Lakshya Bhardwaj <lakshyasharma6258@gmail.com>
2026-06-01 21:41:25 +0530

    Create buy_and_sell_stocks.java

---
+++ Week_1/Day_1/buy_and_sell_stocks.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int maxprofit=0;
+        for(int i = 0;i<prices.length;i++){
+            if(min>prices[i]){
+                min = prices[i];
+            }
+            else if(prices[i]-min > maxprofit){
+                maxprofit = prices[i] - min;
+            }
+        }
+        return maxprofit;
+    }
+}
